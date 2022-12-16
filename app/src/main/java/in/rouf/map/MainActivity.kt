8d8899b852008bbab80793e22f7b6dd377fd1367
roufsyed99 <roufsyed99@gmail.com>
2022-12-16 23:00:06 +0530

    Initial commit

---
+++ app/src/main/java/in/rouf/map/MainActivity.kt
@@ -0,0 +1,13 @@
+package `in`.rouf.map
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
